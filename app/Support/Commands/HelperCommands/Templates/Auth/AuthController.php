b6f4b03dc2f20f722027c4591ce85f4908c2b510
darkterminal <fscodexxx@yahoo.com>
2018-01-28 16:41:23 +0700

    Changes to be committed: 	modified:   app/Support/Commands/GeneratorAuthScaffolding.php					=> Add Logic to create Auth Generator 	modified:   app/Support/Commands/HelperCommands/HelperCommand.php				=> Add Method to View and Add Template 	modified:   app/Support/Commands/HelperCommands/Templates/Auth/AuthController.php		=> Fix redirect after sign up 	modified:   app/Support/Commands/HelperCommands/Templates/Auth/PasswordController.php		=> Add Mothod to recover Password 	new file:   app/Support/Commands/HelperCommands/Templates/Email/forgot-password-email.twig	=> Add new Template Forgot Password Email 	new file:   app/Support/Commands/HelperCommands/Templates/Migrations/userMigrations.php		=> Auto Generate User Migrations 	new file:   app/Support/Commands/HelperCommands/Templates/Models/AuthModel.php			=> Auto Generate User Model 	new file:   app/Support/Commands/HelperCommands/Templates/Partials/flash.twig			=> Add Flash Message 	modified:   app/Support/Commands/HelperCommands/Templates/Routes/router.php			=> Auto Create Route Schema 	new file:   app/Support/Commands/HelperCommands/Templates/authTemp/forgot-password.twig		=> Add new Template Forgot Password 	modified:   app/Support/Commands/HelperCommands/Templates/authTemp/home.twig			=> Auth Homepage 	modified:   app/Support/Commands/HelperCommands/Templates/authTemp/password/change.twig		=> Password Change 	new file:   app/Support/Commands/HelperCommands/Templates/authTemp/password/forgot-password.twig => Forgot Password Form 	new file:   app/Support/Commands/HelperCommands/Templates/authTemp/password/reset-password.twig	=> Reset Password Form 	modified:   app/Support/Commands/HelperCommands/Templates/authTemp/signin.twig			=> Change Sign In 	modified:   app/Support/Commands/HelperCommands/Templates/authTemp/signup.twig			=> Change Sign Up 	new file:   app/Support/Commands/HelperCommands/Templates/email/forgot-password-email.twig	=> Template Email to Send Link Password Recovery 	new file:   app/Support/Commands/HelperCommands/Templates/partials/flash.twig			=> Add Flass Template 	modified:   public/assets/css/app.css								=> Blank CSS 	modified:   resources/views/welcome.twig							=> Welcome Page

--- app/Support/Commands/HelperCommands/Templates/Auth/AuthController.php
+++ app/Support/Commands/HelperCommands/Templates/Auth/AuthController.php
@@ -8,12 +8,21 @@ use Respect\Validation\Validator as v;
 
 class AuthController extends Controller
 {
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     * @return Template HTML(twig) Dashboard User
+     */
     public function index($request, $response)
     {
-        $users  = Users::all();
-        return $this->view->render($response, 'auth/home.twig', ['users' => $users]);
+        return $this->view->render($response, 'auth/home.twig');
     }
 
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     * @return boolean and redicect to Home
+     */
     public function getSignOut($request, $response)
     {
         // Sign Out
@@ -22,13 +31,41 @@ class AuthController extends Controller
         return $response->withRedirect($this->router->pathFor('home'));
     }
 
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     * @return Template HTML(twig) Login Page
+     */
     public function getSignIn($request, $response)
     {
         return $this->view->render($response, 'auth/signin.twig');
     }
     
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     *
+     * First Data will validate using Respect Validation
+     * 
+     * if data not validated, user will redirect back to Sing In Page
+     * otherwise user will attempt to login using Email and Password Credentials
+     * 
+     * if attempt failed user will redirect back to Sing In Page
+     * otherwise if everything is OK, user will redirect to Homepage / Dashboard
+     * 
+     * @return Template HTML(twig) Home Page
+     */
     public function postSignIn($request, $response)
     {
+        $validation = $this->validator->validate($request, [
+            'email' => v::noWhitespace()->notEmpty()->email(),
+            'password' => v::noWhitespace()->notEmpty()
+        ]);
+
+        if($validation->failed()){
+            return $response->withRedirect($this->router->pathFor('auth.signin'));
+        }
+
         $auth = $this->auth->attempt(
             $request->getParam('email'),
             $request->getParam('password')
@@ -42,11 +79,32 @@ class AuthController extends Controller
         return $response->withRedirect($this->router->pathFor('auth.home')); 
     }
 
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     * @return Template HTML(twig) Sign Up Page
+     */
     public function getSignUp($request, $response)
     {
         return $this->view->render($response, 'auth/signup.twig');
     }
     
+    /**
+     * @param  Request Interface
+     * @param  Response Interface
+     *
+     * First Data will validate using Respect Validation
+     * 
+     * if data not validated, user will redirect back to Sing Un Page
+     * otherwise user will be created using Eloquent Detail of Credential follow:
+     * - email
+     * - name
+     * - password
+     *
+     * and user will be attempted to Login automaticly using email and password
+     * 
+     * @return Template HTML(twig) Home Page
+     */
     public function postSignUp($request, $response)
     {
         $validation = $this->validator->validate($request, [
@@ -62,29 +120,14 @@ class AuthController extends Controller
         $user = Users::create([
             'email' => $request->getParam('email'),
             'name' => $request->getParam('name'),
-            'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT)
+            'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+            'password_reset' => ''
         ]);
 
         $this->flash->addMessage('info', 'You have been signed up!');
 
         $this->auth->attempt($user->email, $request->getParam('password'));
 
-        return $response->withRedirect($this->router->pathFor('home'));
-    }
-
-    public function userVerification($request, $response)
-    {
-        $user = Users::where('password', $request->getParam('link'))->first();
-
-        if ($request->getParam('link') == $user->password) {
-            
-            $user->status = 1;
-            $user->save();
-
-            return $this->view->render($response, 'auth/signin.twig');
-
-        }else{
-            return "Code not verified!";
-        }
+        return $response->withRedirect($this->router->pathFor('auth.home'));
     }
 }
