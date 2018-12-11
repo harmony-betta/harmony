fa51fc393181f3d91ba1d89a15fb1c0193aec795
Imam Ali Mustofa <d.darkterminal@gmail.com>
2018-12-11 08:19:30 +0700

    Change artisan, version, composer, fix helpers

--- app/Support/Helpers/RegisterHelpers.php
+++ app/Support/Helpers/RegisterHelpers.php
@@ -6,5 +6,5 @@ $view->getEnvironment()->addFilter($twigCustomFilter);
 // Register function custom twig
 $view->getEnvironment()->addFunction($now);
 $view->getEnvironment()->addFunction($date);
-$view->getEnvironment()->addFunction($asset);
+$view->getEnvironment()->addFunction($app_name);
 $view->getEnvironment()->addFunction($public);
