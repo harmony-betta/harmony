40348a04d84d5e53b5e6b5fc7cdd05fbc9d10760
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-08-28 02:51:07 +0700

    build: remove gulp and use tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/assets/**/*.js",
+    "./resources/**/*.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
